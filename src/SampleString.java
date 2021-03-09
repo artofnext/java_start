6936703d3e4b5d1298ae66158569fa69b5eb3035
artofnext <novnovalex@gmail.com>
2021-03-09 19:41:50 +0000

    string example

---
+++ src/SampleString.java
@@ -0,0 +1,14 @@
+
+public class SampleString{
+	public static void main(String[] args){
+	//String Concatenation
+	String str1 = "Rock";
+	String str2 = "Star";
+	//Method 1 : Using concat
+	String str3 = str1.concat(str2);
+	System.out.println(str3);
+	//Method 2 : Using "+" operator
+	String str4 = str1 + str2;
+	System.out.println(str4);
+	}
+}
