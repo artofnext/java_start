b70eed262b49aa5ec75fcb524f31410b451356d5
artofnext <novnovalex@gmail.com>
2021-03-09 19:51:35 +0000

    using some string methods

--- src/SampleString.java
+++ src/SampleString.java
@@ -1,4 +1,4 @@
-
+// https://www.guru99.com/java-strings.html
 public class SampleString{
 	public static void main(String[] args){
 	//String Concatenation
@@ -10,5 +10,18 @@ public class SampleString{
 	//Method 2 : Using "+" operator
 	String str4 = str1 + str2;
 	System.out.println(str4);
+	
+	//Length of a String
+	System.out.println("Length of String: " + str4.length());
+	  
+	System.out.println("Character at position 5: " + str4.charAt(5));
+	//Index of a given character
+	System.out.println("Index of character 'S': " + str4.indexOf('S'));
+	
+	System.out.println("Compare To 'ROcKStar': " + str4.compareTo("ROcKStar"));
+	//Compare to - Ignore case
+	System.out.println("Compare To 'ROCKSTAR' - Case Ignored: " + str4.compareToIgnoreCase("ROCKSTAR"));
+	
+	System.out.println("Replace 'Rock' with 'Duke': " + str4.replace("Rock", "Duke"));
 	}
 }
